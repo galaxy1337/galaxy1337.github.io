d84972ec1f2127abb4b03f32c0c1ea237f778ccf
EmilHugorm <emilkn64@gmail.com>
2017-05-01 19:05:50 +0200

    HHHHH

--- title.js
+++ title.js
@@ -1,14 +1,25 @@
-function loaded() {
-  setInterval(loop, 1200)
+if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
 }
 
-function loop() {
-  document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length]
+function loaded(){
+	
+	setInterval(loop, 600);
+
 }
-document.addEventListener ? document.addEventListener("DOMContentLoaded", function() {
-  loaded()
-}) : document.attachEvent && document.attachEvent("onreadystatechange", function() {
-  loaded()
-});
-var x = 0,
-  titleText = ["C", "C$", "Ch$", "Cha$", "$Cha$", "$Cha", "$Ch", "$C"];
+
+var x = 0;
+
+var titleText = [ "C", "C$", "CH$", "CHA$", "$CHA$", "$CHA", "$CH", "$C"];
+
+function loop(){
+
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
